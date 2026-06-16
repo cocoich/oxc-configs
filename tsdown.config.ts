9a8445700edc45ee758017a8ff48736c1385d7a3
Kim Hokyeong <kim@fb.tapweb.jp>
2026-06-16 19:37:58 +0900

    add tsdown

---
+++ tsdown.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'tsdown'
+
+export default defineConfig({
+  entry: { lint: 'oxlint.config.ts', fmt: 'oxfmt.config.ts' },
+  exports: true,
+  dts: true,
+
+  external: ['oxlint', 'oxfmt'],
+})
